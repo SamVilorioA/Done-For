3ee8da8211c8fe26784a9d999cc1f2674eebcbbb
unknown <samuelvilorio_15@hotmail.com>
2020-06-14 14:55:24 -0400

    Agregar tareas a la lista desde el input text presionando enter

--- source/js/main.js
+++ source/js/main.js
@@ -11,13 +11,16 @@ function AddList(numList = 1)
     list.className = "lista"+numList;
     list.textContent = "Lista #"+numList;
     // ul element
-    ul.className = "ul"+numList;
+    ul.className = 'list-ul';
+    ul.id = 'ul'+numList;
     //li element
-    li.className ="li1";
+    //li.className ="li1";
     //textfield element
     tfield.type = 'type';
     tfield.placeholder = "Introduzca tarea";
-    //checkbox element
+    tfield.id = 'tf'+numList;
+    tfield.className = 'txtInput';
+    /*/checkbox element
     chField.type = 'checkbox';
     chField.id= 'ch1';
     chField.value = 'tarea1';
@@ -27,19 +30,46 @@ function AddList(numList = 1)
     //appending the elements
     li.appendChild(lbl);
     li.appendChild(chField);
-    li.appendChild(document.createElement('br'));
-    li.appendChild(tfield);
-    ul.appendChild(li);
+    //li.appendChild(document.createElement('br'));
+    ul.appendChild(li);*/
     list.appendChild(ul);
+    list.appendChild(tfield);
     lists.appendChild(list);
-    //list.innerHTML += "<li><input type='text' placeholder='Introduzca tarea y presione enter'></li>";
-    //lists.appendChild(list);
-    //alert(  `Funciona ${numList}` );
+    addEventEnter(tfield.id);
+}
+
+function addTask(tf){
+    const tList = tf.previousSibling;
+    const idParent = tList.id;
+    let nli = (tList.getElementsByTagName('li').length + 1);
+    const lbl = document.createElement('label')
+    const chkb = document.createElement('input');
+    const li = document.createElement('li');
+    // li element
+    li.id = `li-${idParent}.${nli}`;
+    // checkbox element
+    chkb.type = 'checkbox';
+    chkb.id = `chk-${idParent}.${nli}`;
+    //label element 
+    lbl.textContent = tf.value;
+    lbl.id = `lbl-${idParent}.${nli}`;
+    lbl.for = chkb.id;
+    //add to the list
+    li.appendChild(chkb);
+    li.appendChild(lbl);
+    tList.appendChild(li);
+    tf.value ='';
+}
+function addEventEnter(id){
+    const tf = document.getElementById(id);
+    tf.addEventListener('keydown', function (e){
+        if(e.keyCode == 13)
+            addTask(tf);
+    } )
 }
 
 function load(){
     const add = document.getElementById("add");
     let nList = 0;
-    //add.onclick(AddList(++nList));
-    add.addEventListener("click", function(){AddList(++nList)});
+    add.addEventListener("click", function(){AddList(++nList)});    
 }
