1453f2cc56a4ac44969a78e365be886593bb6905
unknown <samuelvilorio_15@hotmail.com>
2020-06-23 10:54:26 -0400

    agregar boton de opciones parte 1

--- source/js/main.js
+++ source/js/main.js
@@ -7,31 +7,27 @@ function AddList(numList = 1)
     const tfield = document.createElement('input');
     const chField = document.createElement('input');
     const lbl = document.createElement('label');
+    const hd = document.createElement('span');
+    const opt = document.createElement('label');
     //list element
-    list.className = "lista"+numList;
-    list.textContent = "Lista #"+numList;
+    list.className = 'list-container';
+    list.id = `list-${numList}`;
+    //h5 element
+    hd.textContent = 'Lista #'+numList;
     // ul element
     ul.className = 'list-ul';
     ul.id = 'ul'+numList;
-    //li element
-    //li.className ="li1";
+    //option button element
+    opt.className = 'options';
+    opt.innerHTML= '&hellip;';
+    //opt.onclick = function(){this.parentNode.style.display = 'none';}
     //textfield element
     tfield.type = 'type';
     tfield.placeholder = "Introduzca tarea";
     tfield.id = 'tf'+numList;
     tfield.className = 'txtInput';
-    /*/checkbox element
-    chField.type = 'checkbox';
-    chField.id= 'ch1';
-    chField.value = 'tarea1';
-    //label element
-    lbl.textContent = 'Tarea #1 ';
-    lbl.for = 'ch1';
-    //appending the elements
-    li.appendChild(lbl);
-    li.appendChild(chField);
-    //li.appendChild(document.createElement('br'));
-    ul.appendChild(li);*/
+    list.appendChild(hd);
+    list.appendChild(opt);
     list.appendChild(ul);
     list.appendChild(tfield);
     lists.appendChild(list);
@@ -45,18 +41,25 @@ function addTask(tf){
     const lbl = document.createElement('label')
     const chkb = document.createElement('input');
     const li = document.createElement('li');
+    const opts = document.createElement('label');
     // li element
     li.id = `li-${idParent}.${nli}`;
     // checkbox element
     chkb.type = 'checkbox';
     chkb.id = `chk-${idParent}.${nli}`;
-    //label element 
+    chkb.className = 'checkbox';
+    //lbl element 
     lbl.textContent = tf.value;
     lbl.id = `lbl-${idParent}.${nli}`;
-    lbl.for = chkb.id;
+    lbl.for = `${chkb.id}`;
+    //options button
+    opts.innerHTML = '&hellip;';
+    opts.className = 'taskOptions';
+    //erasebtn.onclick = function(){this.parentNode.style.display = 'none';};
     //add to the list
     li.appendChild(chkb);
     li.appendChild(lbl);
+    li.appendChild(opts);
     tList.appendChild(li);
     tf.value ='';
 }
