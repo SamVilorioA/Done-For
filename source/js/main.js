c4fba7ff7ac02e33c7a9b62eb5460377c2980808
unknown <samuelvilorio_15@hotmail.com>
2020-07-06 12:04:16 -0400

    editar y eliminar listas completado

--- source/js/main.js
+++ source/js/main.js
@@ -4,7 +4,7 @@ function AddList(numList = 1)
     let list = document.createElement("span");
     const ul = document.createElement('ul');
     const li = document.createElement('li');
-    const tfield = document.createElement('input');
+    const tfield = document.createElement('input');// los inputs se podrian reducir a un solo y que los demas hereden de el
     const chField = document.createElement('input');
     const lbl = document.createElement('label');
     const hd = document.createElement('span');
@@ -12,20 +12,22 @@ function AddList(numList = 1)
     //list element
     list.className = 'list-container';
     list.id = `list-${numList}`;
-    //h5 element
-    hd.textContent = 'Lista #'+numList;
+    //title element
+    hd.id =  `lt-${numList}`;
+    hd.textContent = `Lista #${numList}`;
     // ul element
     ul.className = 'list-ul';
-    ul.id = 'ul'+numList;
+    ul.id = `ul-${numList}`;
     //option button element
     opt.className = 'options';
-    opt.innerHTML= /*'&hellip;';*/ '<i class="fas fa-ellipsis-h"></i>';
+    opt.innerHTML= '&hellip;';// '<i class="fas fa-ellipsis-h"></i>';
     //opt.onclick = function(){this.parentNode.style.display = 'none';}
     //textfield element
-    tfield.type = 'type';
+    tfield.type = 'text';
     tfield.placeholder = "Introduzca tarea";
-    tfield.id = 'tf'+numList;
+    tfield.id = `tf${numList}`;
     tfield.className = 'txtInput';
+    //appending elements
     list.appendChild(hd);
     list.appendChild(opt);
     list.appendChild(ul);
@@ -36,40 +38,90 @@ function AddList(numList = 1)
 }
 
 function addTask(tf){
-    const tList = tf.previousSibling;
-    const idParent = tList.id;
-    let nli = (tList.getElementsByTagName('li').length + 1);
-    const lbl = document.createElement('label')
-    const chkb = document.createElement('input');
-    const li = document.createElement('li');
-    const opts = document.createElement('label');
-    // li element
-    li.id = `li-${idParent}.${nli}`;
-    // checkbox element
-    chkb.type = 'checkbox';
-    chkb.id = `chk-${idParent}.${nli}`;
-    chkb.className = 'checkbox';
-    //lbl element 
-    lbl.textContent = tf.value;
-    lbl.id = `lbl-${idParent}.${nli}`;
-    lbl.for = `${chkb.id}`;
-    //options button
-    opts.innerHTML = /*'&hellip;';*/ '<i class="fas fa-times"></i>'
-    opts.className = 'taskOptions';
-    //erasebtn.onclick = function(){this.parentNode.style.display = 'none';};
-    //add to the list
-    li.appendChild(chkb);
-    li.appendChild(lbl);
-    li.appendChild(opts);
-    tList.appendChild(li);
-    tf.value ='';
+    if(tf.value != 0)
+    {    
+        const tList = tf.previousSibling;
+        const idParent = tList.id;
+        let nli = (tList.getElementsByTagName('li').length + 1);
+        const lbl = document.createElement('label')
+        const chkb = document.createElement('input');
+        const li = document.createElement('li');
+        const clb = document.createElement('label');
+        // li element
+        li.id = `li-${idParent}.${nli}`;
+        // checkbox element
+        chkb.type = 'checkbox';
+        chkb.id = `chk-${idParent}.${nli}`;
+        chkb.className = 'checkbox';
+        //lbl element 
+        lbl.textContent = tf.value;
+        lbl.id = `lbl-${idParent}.${nli}`;
+        lbl.for = `${chkb.id}`;
+        //options button
+        clb.innerHTML = '&times;'; //'<i class="fas fa-times"></i>'
+        clb.className = 'taskDelete';
+        clb.id = `cl-${idParent}.${nli}`;
+        //add to the list
+        li.appendChild(chkb);
+        li.appendChild(lbl);
+        li.appendChild(clb);
+        tList.appendChild(li);
+        tf.value ='';
+        addEventClick(clb.id);
+    }
 }
+
+function deleteElement(p_Element)
+{
+    const element = document.getElementById(p_Element);
+    element.remove();
+}
+function editElement(p_Element)
+{
+    const element = document.getElementById(p_Element);
+    const tfield = document.createElement('input');
+    tfield.type = 'text';
+    tfield.id = `e_tf-${p_Element}`;
+    tfield.placeholder = 'Nombre de la lista';
+    tfield.value = element.textContent;
+    element.textContent = '';
+    element.appendChild(tfield);
+    addEventEnter(tfield.id);
+}
+function saveChange(tf)
+{
+    if(tf.value != 0)
+    {
+        const title = tf.parentElement;
+        title.textContent = tf.value;
+        //tf.remove();
+    }
+}
+
 function addEventEnter(id){
-    const tf = document.getElementById(id);
-    tf.addEventListener('keydown', function (e){
+    const element = document.getElementById(id);
+    element.addEventListener('keydown', function (e){
         if(e.keyCode == 13)
-            addTask(tf);
+            if(element.id[0] === 't')
+                addTask(element);
+            else if(element.id[0] === 'e')
+                saveChange(element);
     } )
+}
+
+function addEventClick(id)
+{
+    const element = document.getElementById(id);
+    element.addEventListener('click', function(e){
+            if(element.id[0] === 'c')   //para eliminar tareas individuales
+                deleteElement(element.parentElement.id);
+            else if(element.id[0] === 'd') // para eliminar listas de tareas
+                deleteElement(element.parentElement.parentElement.id);
+            else if(element.id[0] === 'e') // para editar texto
+                editElement(element.parentElement.parentElement.firstChild.id);
+                //modificar el selector para cuando se arregle el collapsible
+        }
+    )
 }
 
 function load(){
@@ -80,18 +132,19 @@ function load(){
 
 function createSubMenu(id){
     const div = document.createElement('div');
-    const img1 = document.createElement('label');
-    const img2 = document.createElement('label');
+    const editB = document.createElement('label');
+    const deleteB = document.createElement('label');
     div.className = 'subMenu';
-    img1.className = 'img';
-    img2.className = 'img';
-    img1.innerHTML = '<i class="far fa-edit ico-menu"></i>';
-    img2.innerHTML = '<i class="far fa-trash-alt ico-menu"></i>';
+    editB.className = 'img far fa-edit ico-menu';
+    editB.id = `ed-${id}`;
+    deleteB.className = 'img far fa-trash-alt ico-menu';
+    deleteB.id = `del-${id}`;
 
-    let subMenu = document.querySelector(`#list-${id}`);
-        subMenu.appendChild(div);
-        div.appendChild(img1);
-        div.appendChild(img2);
-        subMenu.insertBefore(div, document.querySelector(`#ul${id}`));
-        console.log(subMenu);
+    const subMenu = document.querySelector(`#list-${id}`);
+    subMenu.appendChild(div);
+    div.appendChild(editB);
+    div.appendChild(deleteB);
+    subMenu.insertBefore(div, document.querySelector(`#ul-${id}`));
+    addEventClick(deleteB.id);
+    addEventClick(editB.id);
 }
